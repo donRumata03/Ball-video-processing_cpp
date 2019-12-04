169e25868431518bf9d59d0b3bcfbca037ad182c
donRumata03 <lvv-2003@yandex.ru>
2019-12-04 19:36:34 +0300

    2

--- Circling/Circle finder.cpp
+++ Circling/Circle finder.cpp
@@ -1,26 +1,47 @@
 #include <vector>
 #include <math.h>
+#include <iostream>
 #include <Help_func.cpp>
 
 using namespace std;
 
 
 
-double error_function(list &points, double r, point center){
-    list edges = detect_edges(points);
+double error_function(pms &points, double r, point center){
+    pms edges = detect_edges(points);
     double err = 0;
     for(int i = 0; i < edges.size(); i++){
-        point this_point {edges.at(i)};
-        err += pow(dist(center, ))
+        point this_point {edges.at(i).x, edges.at(i).y};
+        err += pow(dist(center, this_point), POINT_DIST_POW);
     }
+    return err;
 }
 
 
-bool ** make_mt(list &poits){
+bool ** make_mt(pms &points){
+    int minx = INT_MAX, miny = INT_MAX, maxx = INT_MIN, maxy = INT_MIN;
+    for(int i = 0; i < points.size(); i++){
+        minx = min(minx, points.at(i).x);
+        miny = min(miny, points.at(i).y);
+        maxx = max(minx, points.at(i).x);
+        maxy = max(maxy, points.at(i).y);
+    }
+
+    #if DEBUG_MODE
+    cout << "Max x: " << maxx << endl;
+    cout << "Max y: " << maxy << endl;
+    cout << "Min x: " << minx << endl;
+    cout << "Min y: " << miny << endl;
+    #endif
+
+    int mt_h = maxy - miny + 1;
+    int mt_w = maxx - minx + 1;
+    point first {minx, miny};
+    bool ** mt = new bool* [mt_w];
     
 }
 
 
-list detect_edges(list &points){
+pms detect_edges(pms &points){
     
 }
--- Help_func.cpp
+++ Help_func.cpp
@@ -9,13 +9,32 @@ using namespace std;
 
 // Constants
 #define POINT_DIST_POW (1/10)
+#define DEBUG_MODE 1
 
 // Functions
 #define sqr(x) (x * x)
 #define dist(a, b) (sqrt(sqr(a.x - b.x) + sqr(a.y - b.x)))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define min(a,b) (((a) < (b)) ? (a) : (b))
 
 
 struct point{
-    int x;
-    int y;
+    double x;
+    double y;
+};
+
+
+class point_matrix{
+    public:
+        bool ** mt;
+        double h;
+        double w;
+        double first_x;
+        double last_x;
+        double first_y;
+        double last_y;
+
+        point_matrix(int _h, int _w, point first_p){
+            
+        }
 };
